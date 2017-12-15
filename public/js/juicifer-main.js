99364b3c48a38adc4f313659c6a4085f141fc95e
Sofia Ellegren <sofiaellegren@Sofias-MacBook-Air-4.local>
2017-12-15 16:36:39 +0100

    en klass tillagt i varje order-item

--- public/js/juicifer-main.js
+++ public/js/juicifer-main.js
@@ -7,7 +7,8 @@ var socket = io();
 
 Vue.component('order-item', {
   props: ['uiLabels', 'order', 'orderId', 'lang'],
-  template: '<div>{{orderId}} {{order.type}} {{uiLabels.ingredients}}: {{ order.ingredients.map(item=>item["ingredient_"+ lang]).join(", ") }} </div>'
+    //ändrat här också!!!
+  template: '<div class="listItem">{{orderId}} {{order.type}} {{uiLabels.ingredients}}: {{ order.ingredients.map(item=>item["ingredient_"+ lang]).join(", ") }} </div>'
 });
 
 // Stuff that is used both in the ordering system and in the kitchen
