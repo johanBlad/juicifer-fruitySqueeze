957cf622f474eb19abcebf865a339f4caec21f00
Sofia Ellegren <sofiaellegren@Sofias-MacBook-Air-4.local>
2018-01-06 00:22:32 +0100

    ändringar i stock i kitchen-viewn

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -33,6 +33,78 @@ Vue.component('order-item-to-prepare', {
   }
 });
 
+
+Vue.component('stockItem', {
+  props: ['item', 'type', 'lang'],
+  template: ' <div class="ingredient">\
+                  <label>\
+                    {{item["ingredient_"+ lang]}}\
+                  </label>\
+                  <div style="float: right;">\
+                  <label>\
+                    {{ counter }}\
+                  </label>\
+                    <button class="minusButton" v-on:click="decreaseCounter">-</button>\
+                    <button class="plusButton"  v-on:click="incrementCounter">+</button>\
+                    <label style="margin-left: 10px;">\
+                      {{item.selling_price}}:-\
+                    </label>\
+                  </div>\
+              </div>',
+  data: function () {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1;
+      this.$emit('increment');
+    },
+    resetCounter: function () {
+      this.counter = 0;
+    },
+    decreaseCounter: function () {
+      if (this.counter != 0) {
+        this.counter -= 1;
+        this.$emit('decrement');
+      }
+    }
+  }
+});
+  
+
+var vm = new Vue({
+    el: 'stock-wrapper',
+    mixins: [sharedVueStuff],
+    data:{
+        productName: 'dummyName',
+        volume: 0,
+        ingredientsStock:[],
+    },
+    methods:{
+        getIngredientById: function (id) {
+        for (var i =0; i < this.ingredients.length; i += 1) {
+        if (this.ingredients[i].ingredient_id === id){
+        return this.ingredients[i];
+        }
+      }
+    },
+        getIngredientNameList: function (idArr) {
+            return idArr.map(function(id) {
+            return this.getIngredientById(id)["ingredient_" + this.lang];
+      }.bind(this)).join(", ");
+    },
+        getItemQuantityByID: function (idArr) {
+            return idArr.map(function(id) {
+            return this.getIngredientById(id)["vol_smoothie"+"vol_juice"];
+            }.bind(this)).join(", ");
+            }
+            
+        }
+
+    })
+
 //Göra en ny vue component som heter typ total order, för alla objekt som är i en order
 
 var vm = new Vue({
@@ -90,3 +162,29 @@ function checkTime(i) {
     return i;
 };
 
+//cirkeldiagram
+
+/*
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('smoothieCircle'));
+
+        chart.draw(data, options);
+      }
+*/
