992dcee9480d9a0b14782de76273af91312df2e3
Adam Hagevall <adamhagevall@nl110-96-156.student.uu.se>
2018-01-05 17:11:28 +0100

    Lagt till change size-button

--- public/js/ordering.js
+++ public/js/ordering.js
@@ -257,4 +257,8 @@ function checkTime(i) {
     return i;
 };
 
-
+// When the user clicks on div, open the popup
+function popupFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
