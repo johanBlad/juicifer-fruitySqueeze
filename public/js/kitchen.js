598ef9a952325945b659b2654c1b1723d9612201
Sofia Ellegren <sofiaellegren@student242-99.eduroam.uu.se>
2018-01-10 11:32:18 +0100

    enter och backspace funkar

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -1,6 +1,7 @@
 /*jslint es5:true, indent: 2 */
 /*global sharedVueStuff, Vue, socket */
 'use strict';
+var index = 0;
 
 Vue.component('order-item-to-prepare', {
   props: ['uiLabels', 'order', 'orderId', 'lang'],
@@ -86,8 +87,7 @@ var vm = new Vue({
             }.bind(this)).join(", ");
             }
             
-        
-    },
+        }
 
     })
 
@@ -202,34 +202,21 @@ var vm = new Vue({
                this.showHistory();
           }
         }
-        //om enter är tryckt på
-          if (evt.keyCode === 13){
-            if( this.currentOrdersShown){
-                
-              this.markDone(key);
-            }
-          }
-          //om nedåtknappen är tryckt på och sedan enter
-         // else if(evt.keyCode === 13 && evt.keyCode === 40){
-              
-          //}
-        /*
-          else if(evt.keyCode === 8){
-            if(this.currentOrdersShown){
-              this.cancelOrder();
-            }
-          }
-          */
-        //om backspace är tryckt på
-          
-      },
-      checkStockAmount: function () {
-            if (item.vol_smoothie + item.vol_juice < 6){
-                return true;
-            }
-    
-        
+        if (evt.keyCode === 13){
+              /*var all_orders= this.getOrders();
+              var firstOrder = all_orders[0];*/
+            var key = Object.keys(this.orders)[index];
+            console.log('key: ',key);
+            this.markDone(key);
+            index = index+1;
+            
+        if (evt.keyCode === 8){
+            var key = Object.keys(this.orders)[index];
+            this.sendCancel(key);
+            index = index+1;
+        }
       }
+    }
       
     },
     
